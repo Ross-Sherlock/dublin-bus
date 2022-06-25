ba2515a1fb1acbf633dc6693607f22e0fa211a24
Ross-Sherlock <65044806+Ross-Sherlock@users.noreply.github.com>
2022-06-25 17:10:14 +0100

    Display routes on search

--- sevenam-db/src/components/floatingWindow/JourneyForm.js
+++ sevenam-db/src/components/floatingWindow/JourneyForm.js
@@ -1,39 +1,17 @@
 import "./JourneyForm.css";
 import Card from "../UI/Card";
-import { useState } from "react";
-import { Autocomplete } from "@react-google-maps/api";
-
+import { useState, useRef } from "react";
+import { Autocomplete, DirectionsRenderer } from "@react-google-maps/api";
 
 const JourneyForm = (props) => {
-  const [enteredStart, setEnteredStart] = useState("");
-  const startChangeHandler = (event) => {
-    setEnteredStart(event.target.value);
-  };
-
-  const [enteredDest, setEnteredDest] = useState("");
-  const destChangeHandler = (event) => {
-    setEnteredDest(event.target.value);
-  };
+  const calcRoute = props.calcRoute;
+  const clearRoute = props.clearRoute;
+  const originRef = props.originRef;
+  const destinationRef = props.destinationRef;
 
   const [enteredDate, setEnteredDate] = useState("");
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-
-  const journeyData = {
-    start: enteredStart,
-    dest: enteredDest,
-    date: new Date(enteredDate)
-  }
-
-  props.onSubmitJourneyData(journeyData);
-  setEnteredStart("");
-  setEnteredDest("");
-  setEnteredDate("");
-
   };
 
   const map = props.map;
@@ -42,43 +20,57 @@ const JourneyForm = (props) => {
 
   const submitRecentre = () => {
     map.panTo(centre);
-  }
-
-
+  };
 
   return (
-    <form onSubmit={submitHandler}>
       <div className="window-container">
         <div className="journey-input">
           <label>From</label>
-          <Autocomplete options={{
-    componentRestrictions: { country: "ie" },
-  }}>
-          <input
-            type="text"
-            value={enteredStart}
-            onChange={startChangeHandler}
-          />
+          <Autocomplete
+            options={{
+              componentRestrictions: { country: "ie" },
+            }}
+          >
+            <input
+              type="text"
+              ref={originRef}
+            />
           </Autocomplete>
         </div>
         <div className="journey-input">
           <label>To</label>
-          <Autocomplete options={{
-    componentRestrictions: { country: "ie" },
-  }}>
-          <input type="text" value={enteredDest} onChange={destChangeHandler} />
+          <Autocomplete
+            options={{
+              componentRestrictions: { country: "ie" },
+            }}
+          >
+            <input
+              type="text"
+              ref={destinationRef}
+            />
           </Autocomplete>
         </div>
         <div className="journey-input">
           <label>Date</label>
-          <input type="date" onChange={dateChangeHandler} value={enteredDate} />
+          <input type="date"/>
         </div>
         <div className="journey-submit">
-        <button className='recentre' onClick={submitRecentre}><img src="https://cdn1.iconfinder.com/data/icons/ios-edge-line-6/25/Location-Arrow-Crcle-512.png" width="40" height="auto" alt="submit" /></button>
-          <button type="submit" class="btn btn-primary">Search</button>
+          <button className="recentre" onClick={submitRecentre}>
+            <img
+              src="https://cdn1.iconfinder.com/data/icons/ios-edge-line-6/25/Location-Arrow-Crcle-512.png"
+              width="40"
+              height="auto"
+              alt="submit"
+            />
+          </button>
+          <button class="btn btn-primary" onClick={calcRoute}>
+            Search
+          </button>
+          <button class="btn btn-secondary" onClick={clearRoute}>
+            Search
+          </button>
         </div>
       </div>
-    </form>
   );
 };
 
--- sevenam-db/src/components/map/Map.js
+++ sevenam-db/src/components/map/Map.js
@@ -3,8 +3,10 @@ import {
   GoogleMap,
   Marker,
   Autocomplete,
+  DirectionsRenderer,
+  dir
 } from "@react-google-maps/api";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Map.css";
 import JourneyForm from "../floatingWindow/JourneyForm";
 
@@ -17,6 +19,38 @@ const Map = () => {
     libraries: ["places"],
   });
 
+  // Render directions
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const originRef = useRef();
+
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destinationRef = useRef();
+
+  async function calcRoute() {
+    if (originRef.current.value === "" || destinationRef.current.value === "") {
+      return;
+    }
+    const directionsService = new window.google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destinationRef.current.value,
+      travelMode: window.google.maps.TravelMode.TRANSIT,
+    });
+    setDirectionsResponse(results);
+  }
+
+  async function clearRoute() {
+    setDirectionsResponse(null);
+    setDistance("");
+    setDuration("");
+    originRef.current.value = "";
+    destinationRef.current.value = "";
+  }
+
   if (!isLoaded) {
     return <h1>Loading</h1>;
   }
@@ -28,8 +62,18 @@ const Map = () => {
         zoom={15}
         mapContainerStyle={{ width: "100%", height: "100%" }}
         onLoad={(map) => setMap(map)}
-      ></GoogleMap>
-      <JourneyForm map={map} setMap={setMap} centre={centre}></JourneyForm>
+      >
+        {directionsResponse && <DirectionsRenderer directions={directionsResponse} />}
+      </GoogleMap>
+      <JourneyForm
+        map={map}
+        setMap={setMap}
+        centre={centre}
+        clearRoute={clearRoute}
+        calcRoute={calcRoute}
+        originRef={originRef}
+        destinationRef={destinationRef}
+      ></JourneyForm>
     </div>
   );
 };
