5135ba36d72db64f9785ec3b17e04cbb3439c85e
Estellerl <estellerl@icloud.com>
2022-07-01 11:07:18 +0100

    Create MobileApp.js

---
+++ sevenam-db/src/MobileApp.js
@@ -0,0 +1,15 @@
+
+import React from react;
+
+
+
+function MobileApp() {
+ 
+  return (
+    <div className="MobileApp">
+      <h1> Hello world</h1>
+    </div>
+  );
+}
+
+export default MobileApp;
