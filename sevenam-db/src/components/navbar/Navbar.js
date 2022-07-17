735b85c8b026cc2628023a5be2a60adb193d7483
Ross-Sherlock <65044806+Ross-Sherlock@users.noreply.github.com>
2022-07-17 13:06:19 +0100

    Add response navbar

--- sevenam-db/src/components/navbar/Navbar.js
+++ sevenam-db/src/components/navbar/Navbar.js
@@ -1,39 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./Navbar.css";
 import Weather from "./Weather";
 import { Navigate } from "react-router-dom";
-
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
-const [accessMobileApp, setaccessMobileApp] = React.useState(false)
-if (accessMobileApp) {
-  return <Navigate to="/MobileApp"/>;
-}
-
-
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
 
   return (
-    <nav className="nav">
+    <nav className="nav" id="navbar">
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      ></link>
       <div className="nav-title">
-      <a href="/" id="nav-title-text">
-        SevenAM
-      </a>
+        <a href="/" id="nav-title-text">
+          SevenAM
+        </a>
       </div>
-      <ul>
-        <button onClick={() => {setaccessMobileApp(true)}}>
-          Mobile Option
-        </button>
-        <li>
-          <a href="/AllRoutes">All Routes</a>
-        </li>
-        <li>
-          <a href="/placeholder">News</a>
-        </li>
-        <li>
-        <a href="/about">About Us</a>
-        </li>
-      </ul>
-      <Weather/>
+      <button
+        className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}
+      >
+        <MenuIcon />
+      </button>
+      <div className={isNavExpanded ? "nav-menu.expanded" : "nav-menu"}>
+        <ul>
+          <li>
+            <a href="/AllRoutes">All Routes</a>
+          </li>
+          <li>
+            <a href="/placeholder">News</a>
+          </li>
+          <li>
+            <a href="/about">About Us</a>
+          </li>
+          <div className="weather">
+          <Weather />
+          </div>
+        </ul>
+      </div>
     </nav>
   );
 };
