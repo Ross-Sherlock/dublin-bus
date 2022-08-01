84af8a05c19c3853d7f238ff6b97b1a845bea1ed
ClarenceWhite <48053038+ClarenceWhite@users.noreply.github.com>
2022-08-01 20:57:49 +0100

    map.js conflicts merge

--- sevenam-db/src/components/map/Map.js
+++ sevenam-db/src/components/map/Map.js
@@ -6,11 +6,12 @@ import {
 import React, { useState, useRef } from "react";
 import "./Map.css";
 import JourneyForm from "../floatingWindow/JourneyForm";
-import ToggleVisability from "../UI/ToggleVisibility"
+import ToggleVisability from "../UI/ToggleVisibility";
 
 /*=====================start script=====================*/
 const centre = { lat: 53.343, lng: -6.256 };
 const Map = (props) => {
+/*=====================side-panel toggle=====================*/
   const [isExtended, setIsExtended] = useState(true);
   const setExtend = () => {
     setIsExtended(!isExtended);
@@ -20,10 +21,10 @@ const Map = (props) => {
     if (isExtended) {
       css = `
   .side-panel{
-    display:block !important;
+    display:flex !important;
   }
   .side-panel-toggle{
-    margin-left:19.4%;
+    margin-left: 22%;
   }
   `;
     } else {
@@ -44,7 +45,19 @@ const Map = (props) => {
     }
     return arrow
   }
-
+/*=====================prediction parameters=====================*/
+  const [month, setMonth] = useState(null);
+  const [day, setDay] = useState(null);
+  const [hour, setHour] = useState(null);
+  const [start_lat, setStart_lat] = useState(null);
+  const [start_lng, setStart_lng] = useState(null);
+  const [end_lat, setEnd_lat] = useState(null);
+  const [end_lng, setEnd_lng] = useState(null);
+  const [route_numner, setRoute_number] = useState(null);
+  const [start_stopid, setStart_stopid] = useState(null);
+  const [end_stopid, setEnd_stopid] = useState(null);
+  const [route_index, setRoute_index] = useState(0);
+/*=====================google map=====================*/
   const [map, setMap] = useState(/** @type google.maps.Map */ (null));
   const [libraries] = useState(["places"]);
   const { isLoaded } = useJsApiLoader({
@@ -67,6 +80,7 @@ const Map = (props) => {
 
   const [responseJSON, setResponseJSON] = useState({});
 
+/*=====================calculate route=====================*/
   async function calcRoute() {
     if (originRef.current.value === "" || destinationRef.current.value === "") {
       return;
@@ -86,8 +100,8 @@ const Map = (props) => {
     });
     // Filter routes to only include Dublin Bus
     results.routes = results.routes.filter(checkRoute);
-    // testingRef.current.value = results;
-    console.log(results);
+    console.log("RESULTS:", results);
+    console.log("RESULTS.ROUTES:", results.routes);
     setResponseJSON(results);
     setDirectionsResponse(results);
 
@@ -104,8 +118,84 @@ const Map = (props) => {
       }
       return true;
     }
+
+    function getMDH(departure_time) {
+      const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+      let month = months[departure_time.getMonth()];
+      console.log("MONTH:", month)
+      setMonth(month);
+
+      const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+      let day = days[departure_time.getDay()];
+      console.log("DAY", day)
+      setDay(day);
+
+      let hour = departure_time.getHours();
+      console.log("HOUR:", hour)
+      setHour(hour);
+    }
+    let departure_time = results.request.transitOptions.departureTime;
+    getMDH(departure_time); //get month day hour
+
+    function getDynamicParams(steps){
+      let transit = steps.transit;
+      let arrival_stop = transit.arrival_stop;
+      let departure_stop = transit.departure_stop;
+      let line = transit.line;
+      let start_lat = departure_stop.location.lat;
+      console.log("START_LAT:",start_lat());
+      setStart_lat(start_lat());
+      let start_lng = departure_stop.location.lng;
+      console.log("START_LNG:", start_lng());
+      setStart_lng(start_lng());
+      let end_lat = arrival_stop.location.lat;
+      console.log("END_LAT:", end_lat());
+      setEnd_lat(end_lat());
+      let end_lng = arrival_stop.location.lng;
+      console.log("END_LNG:", end_lng());
+      setEnd_lng(end_lng());
+      let route_number = line.short_name;
+      console.log("ROUTE NUMBER:", route_number);
+      setRoute_number(route_number);
+
+      function check_stop_code(name) {
+        if (name.includes(", stop ")) {
+          let loc_index = name.indexOf(", stop ");
+          loc_index = loc_index+7;
+          let code = name.slice(loc_index, name.length);
+          console.log("FOUND STOP CODE PROVIDED:", code)
+          return parseInt(code)
+        } else{
+          return null;
+        }
+      }
+      let start_stopid = check_stop_code(departure_stop.name);
+      setStart_stopid(start_stopid);
+      let end_stopid = check_stop_code(arrival_stop.name);
+      setEnd_stopid(end_stopid);
+    }
+
+    function get_steps_list(all_steps) {
+      let steps_list = [];
+      for (const step in all_steps) {
+        if (all_steps[step].travel_mode == "TRANSIT") {
+          steps_list.push(all_steps[step]);
+        } else {
+          continue;
+        }
+      }
+      console.log("STEPS LIST:", steps_list);
+      return steps_list;
+    }
+
+    console.log("ROUTE INDEX:", route_index);
+    let all_steps = results.routes[route_index].legs[0].steps;
+    console.log("ALL STEPS IN A ROUTE:", all_steps);
+    getDynamicParams(get_steps_list(all_steps)[0]);
+
   }
 
+/*=====================clear route=====================*/
   function clearRoute() {
     setDirectionsResponse(null);
     setMap(null);
@@ -113,20 +203,19 @@ const Map = (props) => {
     destinationRef.current.value = "";
     document.getElementById("direction-steps").innerHTML = "";
   }
-
-  if (!isLoaded) {
-    return <h1>Loading</h1>;
-  }
-
+/*=====================fav function=====================*/
   async function favRoute() {
     if (directionsResponse) {
       localStorage.setItem("fav", JSON.stringify(responseJSON));
     }
   }
-
+/*=====================return=====================*/
+  if (!isLoaded) {
+    return <h1>Loading</h1>;
+  }
   return (
     <div className="content-container">
-      <ToggleVisability content="side-panel">
+      <style>{getStyle()}</style>
       <div className="side-panel">
         <JourneyForm
           map={map}
@@ -140,9 +229,22 @@ const Map = (props) => {
           setDate={setDate}
           favRoute={favRoute}
           response={responseJSON}
+          month={month}
+          day={day}
+          hour={hour}
+          start_lat={start_lat}
+          start_lng={start_lng}
+          end_lat={end_lat}
+          end_lng={end_lng}
+          route_number={route_numner}
+          start_stopid={start_stopid}
+          end_stopid={end_stopid}
         ></JourneyForm>
       </div>
-      </ToggleVisability>
+
+      <button className="side-panel-toggle" id="side-panel-trigger" type="button" onClick={setExtend}>
+        <span className={changeArrow()[0]}>{changeArrow()[1]}</span>
+      </button>
 
       <GoogleMap
         center={centre}
